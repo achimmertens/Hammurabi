f8d7fb8ff4ebf6bfc0335e00179678e74044798e
achimmertens <Ranzi@t-online.de>
2021-11-10 08:22:07 +0100

    case Fälle hinzugefügt

--- src/hamurabi/Hamurabi.java
+++ src/hamurabi/Hamurabi.java
@@ -6,7 +6,7 @@ package hamurabi;
 // Version 2021.Nov.09
 // **************************************************
 
-import java.util.Scanner; // Import the Scanner class
+//import java.util.Scanner; // Import the Scanner class
 
 public class Hamurabi {
 
@@ -15,12 +15,12 @@ public class Hamurabi {
 
 		System.out.println("Hamurabi");
 		
-		float treasure = 100.00f;
-		long population =100;
+		double treasure = 100.00f;
+		double population =100;
 		float tax=5;
-		double food=100;
+		long food=100;
 		int year=0;
-		float score = 0;
+		double score = 0;
 		double harvest=0;
 
 		
@@ -33,7 +33,7 @@ public class Hamurabi {
 		{
 		
 			@SuppressWarnings("resource")
-			Scanner myTax = new Scanner(System.in); // Create a Scanner object
+//			Scanner myTax = new Scanner(System.in); // Create a Scanner object
 			/* HelloWorld.main(args); */
 			Event myObj = new Event();
 			myObj.event();
@@ -43,9 +43,11 @@ public class Hamurabi {
 
 			
 			
-		    System.out.println("Type in the new taxrate for the year "+year+": ");
+//		    System.out.println("Type in the new taxrate for the year "+year+": ");
+			String input="Type in the new taxrate for the year " + year+": ";
+			tax = Float.valueOf(javax.swing.JOptionPane.showInputDialog(input)); 
 			
-		    tax = myTax.nextInt(); // Read user input
+
 		    harvest = Math.round((population)*0.7 + population*0.3*(Math.random()+0.5));
 		    treasure = treasure + tax;
 			food = Math.round(food - population + harvest + treasure*0.1 - tax) ;
@@ -53,10 +55,40 @@ public class Hamurabi {
 			score = score + treasure/year;
 			
 			System.out.println("This year we had "+myObj.ev); //Hier wird ein Attribut einer Instanz/ eines Objekts aufgerufen
-		    if (myObj.ev == "a famine") {
-		    	harvest = harvest * myObj.impact/100;
-		    	System.out.println("Your harvest was reduced to: " +harvest);
-		  		    }
+//		    if (myObj.ev == "a famine") {
+//		    	harvest = harvest * myObj.impact/100;
+//		    	System.out.println("Your harvest was reduced to: " +harvest);
+//		  		    }
+		    
+		    
+		    switch (myObj.ev){
+            case "a famine":
+            	System.out.println("The impact was "+myObj.impact+"%");
+            	harvest = harvest * myObj.impact/100;
+            	System.out.println("Your harvest was reduced to: " +harvest);
+            case "an attac":
+            	System.out.println("The impact was "+myObj.impact+"%");
+            	harvest = harvest * myObj.impact/100;
+                population = population * myObj.impact/100;
+                treasure = treasure * myObj.impact/100;
+                System.out.println("Your harvest was reduced to: " +harvest);
+                System.out.println("Your population was reduced to: " +population);
+                System.out.println("Your treasure was reduced to: " +treasure);
+            case "an earthquake":
+            	System.out.println("The impact was "+myObj.impact+"%");
+            	population = population * myObj.impact/100;
+                treasure = treasure * myObj.impact/100;
+                System.out.println("Your population was reduced to: " +population);
+                System.out.println("Your treasure was reduced to: " +treasure);
+            case "an epidemy":
+            	System.out.println("The impact was "+myObj.impact+"%");
+            	population = population * myObj.impact/100;
+                System.out.println("Your harvest was reduced to: " +harvest);
+                System.out.println("Your population was reduced to: " +population);
+                System.out.println("Your treasure was reduced to: " +treasure);
+            	
+            }
+		    
 		    
 			System.out.println("The Treasure is: "+treasure+", the harvest was "+harvest+", the foodstorage is: "+food+", the population is: "+population + ", your score is: "+score);
 			

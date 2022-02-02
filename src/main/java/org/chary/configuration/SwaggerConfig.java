4e7da1c6bc9806a9fe87e5fce4ba3df712e09416
achimmertens <Ranzi@t-online.de>
2022-02-02 14:55:40 +0100

    swagger installed

---
+++ src/main/java/org/chary/configuration/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package org.chary.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+
+    public static final Contact DEFAULT_CONTACT = new Contact(
+            "Hans-Joachim Blanke", "http://www.4point.de", "blanke@4point.de");
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES =
+            new HashSet<String>(Arrays.asList("application/json","application/xml"));
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(SwaggerConfig.apiInfo())
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+    }
+
+    private static ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Swagger API")
+                .description("Swagger API reference for developers")
+                .version("1.0")
+                .contact(DEFAULT_CONTACT)
+                .termsOfServiceUrl("http://www.4point.de")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0").build();
+    }
+}

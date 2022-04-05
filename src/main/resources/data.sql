606eb8c1543c4fb2923c43077cd84fe720bea8b6
Achim Mertens <achim_mertens@gmx.de>
2022-04-05 14:32:53 +0200

    Springboot from scratch

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO userlog VALUES('1','achimmertens','2022-01-31 14:13:33');
-INSERT INTO userlog VALUES('2','lieselottepulver','2022-01-31 14:16:00');
-INSERT INTO userlog VALUES('3','annewandstehn','2022-01-31 14:17:00');
--- src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE userlog
-(
-      id   VARCHAR(15)  NOT NULL PRIMARY KEY,
-      username VARCHAR(100) NOT NULL,
-      logintime TIMESTAMP NOT NULL
-);

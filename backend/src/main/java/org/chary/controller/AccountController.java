440c9ce3072f6d6b9233e323ce1c43582acb8cf9
Achim Mertens <achim_mertens@gmx.de>
2022-05-17 07:48:53 +0200

    Backend kann jetzt alle Accounts gleichzeitig ausgeben

--- backend/src/main/java/org/chary/controller/AccountController.java
+++ backend/src/main/java/org/chary/controller/AccountController.java
@@ -5,7 +5,10 @@ import org.chary.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/account")
 public class AccountController {
     private final AccountService service;
@@ -24,6 +27,14 @@ public class AccountController {
     public AccountEntity findById(@PathVariable final String id) {
         return service.findById(id);
     }
+
+    @GetMapping("/accounts")
+    public List<Iterable<AccountEntity>> getAllAccounts() {
+        List<Iterable<AccountEntity>> allAccounts = service.getAllAccounts();
+        System.out.println(allAccounts);
+        return (allAccounts);
+    }
 }
+
 
 
--- backend/src/main/java/org/chary/service/AccountService.java
+++ backend/src/main/java/org/chary/service/AccountService.java
@@ -3,12 +3,17 @@ package org.chary.service;
 import org.chary.model.AccountEntity;
 import org.chary.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AccountService {
 
-    private final AccountRepository repository;
+    @Autowired
+    AccountRepository repository;
 
     @Autowired
     public AccountService(AccountRepository repository) {
@@ -22,4 +27,28 @@ public class AccountService {
     public AccountEntity findById(final String id) {
         return repository.findById(id).orElse(null);
     }
+
+
+    public List<Iterable<AccountEntity>> getAllAccounts() {
+        List<Iterable<AccountEntity>> accounts = new ArrayList<>();
+        accounts.add(repository.findAll(Pageable.unpaged()));
+        return (accounts);
+    }
+/*
+
+    public List<AccountEntity> getAllAccounts() {
+        List<AccountEntity> accounts = new ArrayList<>();
+        AccountEntity content = repository.findById("1").orElse(null);
+        accounts.add(content);
+        return (accounts);
+    }
+
+
+       public List<Iterable<AccountEntity>> getAllAccounts() {
+           List<Iterable<AccountEntity>> accounts = new ArrayList<>();
+           Iterable<AccountEntity> content = repository.findAll() ;
+           accounts.add(content);
+           return (accounts);
+       }
+*/
 }

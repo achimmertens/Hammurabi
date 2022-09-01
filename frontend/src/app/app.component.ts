f0cc61faab5cae7c45d3c368ae5b010ddc87f6ae
Achim Mertens <achim_mertens@gmx.de>
2022-09-01 06:30:48 +0200

    Hammurabi HIntergrundbild

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css']
   //styleUrls: ['styles.cf58507dd785c10c7bc8.css']
-  //styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Frontend';

b7a658636f1f8acc9ec0120025f931d4d0f14119
Achim Mertens <achim_mertens@gmx.de>
2022-11-08 07:37:11 +0100

    Logging jetzt sauber eingerichtet. Der Trick: logging.level.org.chary=DEBUG und logging.pattern.console=%d{HH:mm:ss} %-5p  %c{1}:%L - %msg%n in application.properties gesetzt. pom.xml hat keine Zusatzlogger. Alles läuft über slf4j.  private final Logger log = LoggerFactory.getLogger(Config.class);

--- backend/src/main/java/org/chary/config/Config.java
+++ backend/src/main/java/org/chary/config/Config.java
@@ -1,5 +1,7 @@
 package org.chary.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.chary.service.AccountService;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,15 +12,16 @@ import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+
 
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "org.chary.repository")
 @ComponentScan(basePackages = { "org.chary.service" })
 public class Config extends AbstractElasticsearchConfiguration {
-    private static final Logger logger=LogManager.getLogger(Config.class);
+    //private static final Logger logger=LogManager.getLogger(Config.class);
+    private final Logger log = LoggerFactory.getLogger(Config.class);
 
     @Value("${elasticsearch.url}")
     public String elasticsearchUrl;
@@ -27,17 +30,17 @@ public class Config extends AbstractElasticsearchConfiguration {
     @Override
     public RestHighLevelClient elasticsearchClient() {
         try {
-            logger.info("Trying to connect to the elasticSearch database ...");
+            log.info("Trying to connect to the elasticSearch database ...");
             final ClientConfiguration config = ClientConfiguration.builder()
                     .connectedTo(elasticsearchUrl)
                     .build();
-            logger.info("RestClients.create(config).rest() = ",RestClients.create(config).rest());
-            logger.debug("Aber sonst gehts mir gut");
-            logger.info("Aber sonst gehts mir gut, auch als info.");
+            log.info("RestClients.create(config).rest() = ",RestClients.create(config).rest());
+            log.debug("Diese Meldung erscheint nur im Debug Level");
+            log.warn("Diese Meldung erscheint nur im Warn Level");
             return RestClients.create(config).rest();
         }
         catch (RuntimeException exc) {
-            logger.warn("There is a problem with the connection to the elasticSearch database!", exc);
+            log.warn("There is a problem with the connection to the elasticSearch database!", exc);
             return null;
         }
     }

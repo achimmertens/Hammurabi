e7b4863c72f6e00c5b8aa3cf2894ca4f8b38c258
Achim Mertens <achim_mertens@gmx.de>
2022-11-08 08:14:51 +0100

    irgendwelche kleine Änderungen

--- backend/src/main/java/org/chary/config/Config.java
+++ backend/src/main/java/org/chary/config/Config.java
@@ -2,7 +2,6 @@ package org.chary.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.chary.service.AccountService;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,13 +13,10 @@ import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfig
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 
-
-
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "org.chary.repository")
 @ComponentScan(basePackages = { "org.chary.service" })
 public class Config extends AbstractElasticsearchConfiguration {
-    //private static final Logger logger=LogManager.getLogger(Config.class);
     private final Logger log = LoggerFactory.getLogger(Config.class);
 
     @Value("${elasticsearch.url}")
@@ -36,8 +32,10 @@ public class Config extends AbstractElasticsearchConfiguration {
                     .build();
             log.info("RestClients.create(config).rest() = ",RestClients.create(config).rest());
             log.debug("Diese Meldung erscheint nur im Debug Level");
-            log.warn("Diese Meldung erscheint nur im Warn Level");
-            return RestClients.create(config).rest();
+            log.warn("If the Tomcat crashes here, then probably the elasticseach database was not found...");
+            return
+
+                    RestClients.create(config).rest();
         }
         catch (RuntimeException exc) {
             log.warn("There is a problem with the connection to the elasticSearch database!", exc);
